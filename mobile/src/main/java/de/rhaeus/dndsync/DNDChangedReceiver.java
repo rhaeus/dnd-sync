1b24e090fc43e35fbd7f491ef432f65cfc839524
rhaeus <rhaeus.dev@gmail.com>
2021-08-28 22:39:56 +0200

    send sync request to all reachable nodes

--- mobile/src/main/java/de/rhaeus/dndsync/DNDChangedReceiver.java
+++ mobile/src/main/java/de/rhaeus/dndsync/DNDChangedReceiver.java
@@ -18,6 +18,9 @@ import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
@@ -25,27 +28,28 @@ import java.util.concurrent.ExecutionException;
 public class DNDChangedReceiver extends BroadcastReceiver{
     private static final String TAG = "DNDChangedReceiver";
     private static final String DND_SYNC_CAPABILITY_NAME = "dnd_sync";
-    private String syncNodeId = null;
     private static final String DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync";
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        int dndState = mNotificationManager.getCurrentInterruptionFilter();
-        Log.d(TAG, "Current DND state is: " + dndState);
+        if (intent.getAction().equals(NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED)) {
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            int dndState = mNotificationManager.getCurrentInterruptionFilter();
+            Log.d(TAG, "Current DND state is: " + dndState);
 
-        new Thread(new Runnable() {
-            public void run() {
-                sendSyncRequest(context, dndState);
-            }
-        }).start();
+            new Thread(new Runnable() {
+                public void run() {
+                    sendSyncRequest(context, dndState);
+                }
+            }).start();
+        }
     }
 
     private void sendSyncRequest(Context context, int dndState) {
         // https://developer.android.com/training/wearables/data/messages
 
-        // search node for sync
+        // search nodes for sync
         CapabilityInfo capabilityInfo = null;
         try {
             capabilityInfo = Tasks.await(
@@ -60,57 +64,38 @@ public class DNDChangedReceiver extends BroadcastReceiver{
             Log.e(TAG, "oh god2", e);
             return;
         }
+
+        // send request to all reachable nodes
         // capabilityInfo has the reachable nodes with the dnd sync capability
-        Log.d(TAG, "updateDndSyncCapability triggered by button");
-        updateDndSyncCapability(capabilityInfo);
-
-        // send request
-        if (syncNodeId != null) {
-            byte[] data = new byte[2];
-            data[0] = 1; // bedtime mode
-            data[1] = (byte)dndState;
-            Task<Integer> sendTask =
-                    Wearable.getMessageClient(context).sendMessage(syncNodeId, DND_SYNC_MESSAGE_PATH, data);
-            // You can add success and/or failure listeners,
-            // Or you can call Tasks.await() and catch ExecutionException
-            sendTask.addOnSuccessListener(new OnSuccessListener<Integer>() {
-                @Override
-                public void onSuccess(Integer integer) {
-                    Log.d(TAG, "send successful!");
-                }
-            });
-            sendTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.d(TAG, "send failed!");
-                }
-            });
-
-        } else {
+        Set<Node> connectedNodes = capabilityInfo.getNodes();
+        if (connectedNodes.isEmpty()) {
             // Unable to retrieve node with transcription capability
             Log.d(TAG, "Unable to retrieve node with sync capability!");
-        }
-
-    }
-
-    private void updateDndSyncCapability(CapabilityInfo capabilityInfo) {
-
-        Set<Node> connectedNodes = capabilityInfo.getNodes();
-
-        syncNodeId = pickBestNodeId(connectedNodes);
-        Log.d(TAG, "updateDndSyncCapability. syncNodeId = " + syncNodeId);
-    }
-
-    private String pickBestNodeId(Set<Node> nodes) {
-        String bestNodeId = null;
-        // Find a nearby node or pick one arbitrarily
-        for (Node node : nodes) {
-            if (node.isNearby()) {
-                return node.getId();
+        } else {
+            for (Node node : connectedNodes) {
+                if (node.isNearby()) {
+                    byte[] data = new byte[2];
+                    data[0] = 1; // bedtime mode
+                    data[1] = (byte) dndState;
+                    Task<Integer> sendTask =
+                            Wearable.getMessageClient(context).sendMessage(node.getId(), DND_SYNC_MESSAGE_PATH, data);
+                    // You can add success and/or failure listeners,
+                    // Or you can call Tasks.await() and catch ExecutionException
+                    sendTask.addOnSuccessListener(new OnSuccessListener<Integer>() {
+                        @Override
+                        public void onSuccess(Integer integer) {
+                            Log.d(TAG, "send successful!");
+                        }
+                    });
+                    sendTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "send failed!");
+                        }
+                    });
+                }
             }
-            bestNodeId = node.getId();
         }
-        return bestNodeId;
     }
 
 }
